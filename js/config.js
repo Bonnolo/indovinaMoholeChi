61a04cf9532e1fcc65297cdc36f4d313f939ef5b
Bonnolo <fbonetti00@gmail.com>
2023-03-17 10:24:33 +0100

    indent change

--- js/config.js
+++ js/config.js
@@ -1,67 +1,66 @@
 const students = [
-    {
-        name: 'Allam Massinissa',
-        src: 'img/AllamMassinissa.jpg'
-    },
-    {
-        name: 'Bonetti Fabio Alberto',
-        src: 'img/BonettiFabioAlberto.jpg'
-    },
-    {
-        name: 'Caballes Patricia',
-        src: 'img/CaballesPatricia.jpg'
-    },
-    {
-        name: 'Caporale Vita Vittorio',
-        src: 'img/CaporaleVitaVittorio.jpg'
-    },
-    {
-        name: 'Folegatti Margherita',
-        src: 'img/FolegattiMargherita.jpg'
-    },
-    {
-        name: 'Forlani Giacomo',
-        src: 'img/ForlaniGiacomo.jpg'
-    },
-    {
-        name: 'Iannarone Orlando',
-        src: 'img/IannaroneOrlando.jpg'
-    },
-    {
-        name: 'Pagaduan JetherRey',
-        src: 'img/PagaduanJetherRey.jpg'
-    },
-    {
-        name: 'Prodigo Gianluca',
-        src:'img/ProdigoGianluca.jpg'
-    },
-    {
-        name: 'Rudellin Alessandro',
-        src:'img/RudellinAlessandro.jpg'
-    },
-    {
-        name: 'Seri Edoardo',
-        src:'img/SeriEdoardo.jpg'
-    },
-    {
-        name: 'Sommaruga Marco',
-        src:'img/SommarugaMarco.jpg'
-    },
-    {
-        name: 'Tolentino John Marco',
-        src:'img/TolentinoJohnMarco.jpg'
-    },
-    {
-        name: 'Ventura Riccardo',
-        src:'img/VenturaRiccardo.jpg'
-    },
-    {
-        name: 'Zhu Xian Ni Jessica',
-        src:'img/ZhuXianNiJessica.jpg'
-    },
+  {
+    name: "Allam Massinissa",
+    src: "img/AllamMassinissa.jpg",
+  },
+  {
+    name: "Bonetti Fabio Alberto",
+    src: "img/BonettiFabioAlberto.jpg",
+  },
+  {
+    name: "Caballes Patricia",
+    src: "img/CaballesPatricia.jpg",
+  },
+  {
+    name: "Caporale Vita Vittorio",
+    src: "img/CaporaleVitaVittorio.jpg",
+  },
+  {
+    name: "Folegatti Margherita",
+    src: "img/FolegattiMargherita.jpg",
+  },
+  {
+    name: "Forlani Giacomo",
+    src: "img/ForlaniGiacomo.jpg",
+  },
+  {
+    name: "Iannarone Orlando",
+    src: "img/IannaroneOrlando.jpg",
+  },
+  {
+    name: "Pagaduan JetherRey",
+    src: "img/PagaduanJetherRey.jpg",
+  },
+  {
+    name: "Prodigo Gianluca",
+    src: "img/ProdigoGianluca.jpg",
+  },
+  {
+    name: "Rudellin Alessandro",
+    src: "img/RudellinAlessandro.jpg",
+  },
+  {
+    name: "Seri Edoardo",
+    src: "img/SeriEdoardo.jpg",
+  },
+  {
+    name: "Sommaruga Marco",
+    src: "img/SommarugaMarco.jpg",
+  },
+  {
+    name: "Tolentino John Marco",
+    src: "img/TolentinoJohnMarco.jpg",
+  },
+  {
+    name: "Ventura Riccardo",
+    src: "img/VenturaRiccardo.jpg",
+  },
+  {
+    name: "Zhu Xian Ni Jessica",
+    src: "img/ZhuXianNiJessica.jpg",
+  },
 ];
 // number of people MAX 15
-const guesses = 15
+const guesses = 15;
 
-
-export { students, guesses }
+export { students, guesses };
